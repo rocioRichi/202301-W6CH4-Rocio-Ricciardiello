d39271bbf14644efbe0dc9b432ce1c4d5dd166f7
rocioRichi <paragit@gmail.com>
2023-02-23 19:19:14 +0100

    Add server working

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,31 @@
 import http from 'http';
 import url from 'url';
 import * as dotenv from 'dotenv';
-// Import { addCountry } from './countrys';
-
-console.log('hola pepe');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4300; // Puerto definido en el package cuando ejecuto start:mon
+const PORT = process.env.PORT || '4500';
 
-const server = http.createServer((req, resp) => {
-  console.log('Server', req.method, PORT);
-  console.log(process.env.PORT);
-  // Console.log('Server', req.method, PORT);
-  if (!req.url) {
-    return new Error('algo');
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      res.write('Hello world');
+      // getAllKnowledge();
+      break;
+    case 'POST':
+      break;
+    case 'PATCH':
+      break;
+    case 'DELETE':
+      break;
+    default:
+      '';
   }
-  const parseURL = url.parse(req.url);
-  const patname = parseURL.pathname;
 
-  // Con desesstructuración sería lo mismo que:
-  // const{pathname}=url.parse(req.url)
-
-  resp.write('Hello Server GET tu pathname es' + patname);
-  server.emit('error', new Error('Invalid URL'));
-
-  // Resp.write('Hello Server');
-  resp.end();
+  res.end();
 });
 
 server.on('listening', () => {
-  console.log('Estoy escuchando en http://localhost/:' + PORT); // Informándonos como administradores
+  console.log('listening on ' + PORT);
 });
-
-server.on('error', () => {});
-
 server.listen(PORT);
