b3ed331cc89a44d0f18f5cb583f1b80557efee20
rocioRichi <paragit@gmail.com>
2023-02-23 21:13:01 +0100

    Add calculator

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,65 @@ const PORT = process.env.PORT || '4500';
 const server = http.createServer((req, res) => {
   switch (req.method) {
     case 'GET':
-      res.write('Hello world');
+      if (!req.url) {
+        server.emit('error', new Error('Error 404'));
+        return;
+      }
+
+      // if (!url.parse(req.url)) {
+      //   server.emit('error', new Error('Error 404'));
+      //   return;
+      // }
+      const parseURL = url.parse(req.url);
+
+      if (!parseURL.pathname) {
+        server.emit('error', new Error('Error 404'));
+        return;
+      }
+      const pathname = parseURL.pathname;
+      if (!parseURL.query) {
+        server.emit('error', new Error('Error 404'));
+        return;
+      }
+
+      const query = parseURL.query;
+      const parseQuery = query.split('&');
+      const varAwithEqual: string = parseQuery[0];
+      const a = +varAwithEqual.substring(2, 10);
+
+      const varBwithEqual: string = parseQuery[1];
+      const b = +varBwithEqual.substring(2, 10);
+
+      // Con desesstructuración sería lo mismo que:       // const{pathname}=url.parse(req.url)        resp.write('Hello Server GET tu pathname es' + patname);       server.emit('error', new Error('Invalid URL'));
+
+      // res.write('Hello world');
       // getAllKnowledge();
+
+      const calculator = () => {
+        let sum = a + b;
+        let res = a - b;
+        let mult = a * b;
+        let div = a / b;
+
+        return (
+          ` a+b= ` + sum + ` a-b= ` + res + ` a*b= ` + mult + ` a/b= ` + div
+        );
+      };
+
+      calculator();
+
+      res.write(
+        'Hello Server GET tu pathname es' +
+          ' query ' +
+          varAwithEqual +
+          ', variable "a" es igual a ' +
+          a +
+          ', ' +
+          varBwithEqual +
+          ', variable b es igual a ' +
+          b +
+          calculator()
+      );
       break;
     case 'POST':
       break;
